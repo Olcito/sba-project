3718e9b04a5e522432ecf4b6952bf55085b3c517
Olcay <olcayto2001@hotmail.com>
2026-06-19 00:10:58 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Software Build Automation Tools Project</h1>
+    <p>Student: Olcay</p>
+    <p>This application is deployed using Azure Web App and GitHub Actions.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
